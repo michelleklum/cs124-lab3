e870f5e6d84bab60b365229574ce417dfafe1bc1
Zoe Tokheim <zoetokheim@gmail.com>
2022-03-22 01:27:29 -0700

    Hid completed tasks header when there aren't any completed tasks

--- src/SingleListPage/ListOfTasks.js
+++ src/SingleListPage/ListOfTasks.js
@@ -3,17 +3,12 @@ import TaskCard from "./TaskCard";
 
 function ListOfTasks(props) {
   const list = props.data.find((list) => list.id === props.currentListId);
-  const tasksToShow = list.hideCompletedTasks
-    ? props.tasks.filter((task) => !task.isCompleted)
-    : props.tasks;
 
-  const completedTasks = tasksToShow.filter((task) => task.isCompleted);
-  const incompleteTasks = tasksToShow.filter((task) => !task.isCompleted);
+  const completedTasks = props.tasks.filter((task) => task.isCompleted);
+  const incompleteTasks = props.tasks.filter((task) => !task.isCompleted);
 
   // Put incomplete tasks first, and then completed tasks.
   // Within each sublist (i.e., incomplete tasks), sort by date.
-  const sortedTasksToShow = incompleteTasks.concat(completedTasks);
-
   return (
     <div id="list-of-tasks">
       {incompleteTasks.map((task) => (
@@ -27,19 +22,21 @@ function ListOfTasks(props) {
           onEditTask={props.onEditTask}
         />
       ))}
-      <hr />
-      <h3 className = "completed-tasks-header">Completed</h3>
-      {completedTasks.map((task) => (
-        <TaskCard
-          key={task.id}
-          currentListId={props.currentListId}
-          task={task}
-          inMenuMode={props.inMenuMode}
-          onChangePage={props.onChangePage}
-          onChangeTask={props.onChangeTask}
-          onEditTask={props.onEditTask}
-        />
-      ))}
+      {!list.hideCompletedTasks && completedTasks.length > 0 && <div>
+        <hr />
+        <h3 className="completed-tasks-header">Completed</h3>
+        {completedTasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            currentListId={props.currentListId}
+            task={task}
+            inMenuMode={props.inMenuMode}
+            onChangePage={props.onChangePage}
+            onChangeTask={props.onChangeTask}
+            onEditTask={props.onEditTask}
+          />
+        ))}
+      </div>}
     </div>
   );
 }

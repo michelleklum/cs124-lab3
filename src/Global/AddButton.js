7f058160b12cbc907fde8008b264b401d6998b5e
Zoe Tokheim <zoetokheim@gmail.com>
2022-04-06 14:55:21 -0700

    Accessibility global beginning

--- src/Global/AddButton.js
+++ src/Global/AddButton.js
@@ -3,12 +3,18 @@ import "./AddButton.css";
 function AddButton(props) {
 
   function handleClick() {
-    (props.currentPage === "Home") ? props.onChangePage("CreateListPage") 
-    : props.onChangePage("CreateTaskPage")
+    (props.currentPage === "Home") ? props.onChangePage("CreateListPage")
+      : props.onChangePage("CreateTaskPage")
   }
 
   return (
-    <div className="add-button" onClick={handleClick}>
+    <div className="add-button"
+      onClick={handleClick}
+      aria-label={props.addLabel}
+      tabIndex="0"
+      role="button"
+      onKeyDown={(e) => (e.code === "Enter") ? handleClick() : null}
+    >
       <i className="fas fa-plus fa-4x"></i>
     </div>
   );

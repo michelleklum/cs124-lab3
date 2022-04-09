f2a1cc48ce1a55b7e7e3fc7d6d0f5acd4d08af74
mlum123 <michellelum123@gmail.com>
2022-04-09 16:09:55 -0700

    Fix FocusTrap bug for large screen menu where user cannot exit

--- src/LargeScreens/LargeScreenSubpageHeader.js
+++ src/LargeScreens/LargeScreenSubpageHeader.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import "./LargeScreenSubpageHeader.css";
+import FocusTrap from "focus-trap-react";
 import LargeScreenSearchBar from "./LargeScreenSearchBar";
 import LargeScreenAddButton from "./LargeScreenAddButton";
 import ListMenuButton from "../SingleListPage/ListMenuButton";
@@ -26,45 +27,53 @@ function LargeScreenSubpageHeader(props) {
             />
           </Fragment>
         )}
-        <ListMenuButton
-          isLargeScreen={props.isLargeScreen}
-          taskList={props.list}
-          inMenuMode={props.inMenuMode}
-          onChangeMenuMode={props.onChangeMenuMode}
-        />
-        {props.inMenuMode && props.menuModeType === "general" ? (
-          <ListMenu
-            isLargeScreen={props.isLargeScreen}
-            onToggleLargeScreenPopup={props.onToggleLargeScreenPopup}
-            tasks={props.tasks}
-            listMenuType="general"
-            onChangeMenuModeType={props.onChangeMenuModeType}
-            data={props.data}
-            currentListId={props.currentListId}
-            onEditList={props.onEditList}
-            onDeleteCompleted={props.onDeleteCompleted}
-            onDeleteOverdue={props.onDeleteOverdue}
-            onDeleteAllTasks={props.onDeleteAllTasks}
-            onDeleteList={props.onDeleteList}
-            onChangePage={props.onChangePage}
-            onToggleDeleteListAlert={props.onToggleDeleteListAlert}
-            onToggleDeleteTasksAlert={props.onToggleDeleteTasksAlert}
-            onToggleDeleteCompletedAlert={props.onToggleDeleteCompletedAlert}
-            onToggleDeleteOverdueAlert={props.onToggleDeleteOverdueAlert}
-          />
-        ) : null}
-        {props.inMenuMode && props.menuModeType === "sorting" ? (
-          <ListMenu
-            isLargeScreen={props.isLargeScreen}
-            listMenuType="sorting"
-            onChangeMenuModeType={props.onChangeMenuModeType}
-            data={props.data}
-            currentListId={props.currentListId}
-            listTasksPrimarySortField={props.listTasksPrimarySortField}
-            listTasksPrimarySortDirection={props.listTasksPrimarySortDirection}
-            onChangeSort={props.onChangeSort}
-          />
-        ) : null}
+        <FocusTrap>
+          <div>
+            <ListMenuButton
+              isLargeScreen={props.isLargeScreen}
+              taskList={props.list}
+              inMenuMode={props.inMenuMode}
+              onChangeMenuMode={props.onChangeMenuMode}
+            />
+            {props.inMenuMode && props.menuModeType === "general" ? (
+              <ListMenu
+                isLargeScreen={props.isLargeScreen}
+                onToggleLargeScreenPopup={props.onToggleLargeScreenPopup}
+                tasks={props.tasks}
+                listMenuType="general"
+                onChangeMenuModeType={props.onChangeMenuModeType}
+                data={props.data}
+                currentListId={props.currentListId}
+                onEditList={props.onEditList}
+                onDeleteCompleted={props.onDeleteCompleted}
+                onDeleteOverdue={props.onDeleteOverdue}
+                onDeleteAllTasks={props.onDeleteAllTasks}
+                onDeleteList={props.onDeleteList}
+                onChangePage={props.onChangePage}
+                onToggleDeleteListAlert={props.onToggleDeleteListAlert}
+                onToggleDeleteTasksAlert={props.onToggleDeleteTasksAlert}
+                onToggleDeleteCompletedAlert={
+                  props.onToggleDeleteCompletedAlert
+                }
+                onToggleDeleteOverdueAlert={props.onToggleDeleteOverdueAlert}
+              />
+            ) : null}
+            {props.inMenuMode && props.menuModeType === "sorting" ? (
+              <ListMenu
+                isLargeScreen={props.isLargeScreen}
+                listMenuType="sorting"
+                onChangeMenuModeType={props.onChangeMenuModeType}
+                data={props.data}
+                currentListId={props.currentListId}
+                listTasksPrimarySortField={props.listTasksPrimarySortField}
+                listTasksPrimarySortDirection={
+                  props.listTasksPrimarySortDirection
+                }
+                onChangeSort={props.onChangeSort}
+              />
+            ) : null}
+          </div>
+        </FocusTrap>
       </div>
     </div>
   );
--- src/SingleListPage/ListMenu.js
+++ src/SingleListPage/ListMenu.js
@@ -1,6 +1,5 @@
 import { Fragment } from "react";
 import "./ListMenu.css";
-import FocusTrap from "focus-trap-react";
 import EditListBar from "./EditListBar";
 import HideCompletedBar from "./HideCompletedBar";
 import SortBar from "./SortBar";
@@ -20,118 +19,104 @@ function ListMenu(props) {
   return (
     <Fragment>
       {props.listMenuType === "general" && (
-        <FocusTrap>
-          <div
-            className={`single-list-page-menu general-menu ${screenSizeClassName}`}
-          >
-            <EditListBar
-              taskList={taskList}
-              onChangePage={props.onChangePage}
-              isLargeScreen={props.isLargeScreen}
-              onToggleLargeScreenPopup={props.onToggleLargeScreenPopup}
-            />
-            <HideCompletedBar
-              taskList={taskList}
-              hideCompletedTasks={taskList.hideCompletedTasks}
-              currentListId={props.currentListId}
-              onEditList={props.onEditList}
-            />
-            <SortBar
-              taskList={taskList}
-              onChangeMenuModeType={props.onChangeMenuModeType}
-            />
-            <DeleteOverdueBar
-              taskList={taskList}
-              onDeleteOverdue={props.onDeleteOverdue}
-              onToggleDeleteOverdueAlert={props.onToggleDeleteOverdueAlert}
-            />
-            <DeleteCompletedBar
-              taskList={taskList}
-              onDeleteCompleted={props.onDeleteCompleted}
-              onToggleDeleteCompletedAlert={props.onToggleDeleteCompletedAlert}
-            />
-            <DeleteAllTasksBar
-              taskList={taskList}
-              onToggleDeleteTasksAlert={props.onToggleDeleteTasksAlert}
-            />
-            <DeleteListBar
-              taskList={taskList}
-              onToggleDeleteListAlert={props.onToggleDeleteListAlert}
-            />
-          </div>
-        </FocusTrap>
+        <div
+          className={`single-list-page-menu general-menu ${screenSizeClassName}`}
+        >
+          <EditListBar
+            taskList={taskList}
+            onChangePage={props.onChangePage}
+            isLargeScreen={props.isLargeScreen}
+            onToggleLargeScreenPopup={props.onToggleLargeScreenPopup}
+          />
+          <HideCompletedBar
+            taskList={taskList}
+            hideCompletedTasks={taskList.hideCompletedTasks}
+            currentListId={props.currentListId}
+            onEditList={props.onEditList}
+          />
+          <SortBar
+            taskList={taskList}
+            onChangeMenuModeType={props.onChangeMenuModeType}
+          />
+          <DeleteOverdueBar
+            taskList={taskList}
+            onDeleteOverdue={props.onDeleteOverdue}
+            onToggleDeleteOverdueAlert={props.onToggleDeleteOverdueAlert}
+          />
+          <DeleteCompletedBar
+            taskList={taskList}
+            onDeleteCompleted={props.onDeleteCompleted}
+            onToggleDeleteCompletedAlert={props.onToggleDeleteCompletedAlert}
+          />
+          <DeleteAllTasksBar
+            taskList={taskList}
+            onToggleDeleteTasksAlert={props.onToggleDeleteTasksAlert}
+          />
+          <DeleteListBar
+            taskList={taskList}
+            onToggleDeleteListAlert={props.onToggleDeleteListAlert}
+          />
+        </div>
       )}
       {props.listMenuType === "sorting" && (
-        <FocusTrap>
-          <div
-            className={`single-list-page-menu sorting-menu ${screenSizeClassName}`}
-          >
-            <SortByHeaderBar
-              taskList={taskList}
-              onChangeMenuModeType={props.onChangeMenuModeType}
-            />
-            <SortByFieldBar
-              taskList={taskList}
-              onChangeSort={props.onChangeSort}
-              listTasksPrimarySortField={props.listTasksPrimarySortField}
-              listTasksPrimarySortDirection={
-                props.listTasksPrimarySortDirection
-              }
-              barSortIcon="fas fa-exclamation-circle"
-              barSortFieldAbbrev="priority"
-              barSortField="priority"
-              barSortFieldText="priority"
-            />
-            <SortByFieldBar
-              taskList={taskList}
-              onChangeSort={props.onChangeSort}
-              listTasksPrimarySortField={props.listTasksPrimarySortField}
-              listTasksPrimarySortDirection={
-                props.listTasksPrimarySortDirection
-              }
-              barSortIcon="fas fa-hourglass-start"
-              barSortFieldAbbrev="deadline"
-              barSortField="deadline"
-              barSortFieldText="deadline"
-            />
-            <SortByFieldBar
-              taskList={taskList}
-              onChangeSort={props.onChangeSort}
-              listTasksPrimarySortField={props.listTasksPrimarySortField}
-              listTasksPrimarySortDirection={
-                props.listTasksPrimarySortDirection
-              }
-              barSortIcon="fas fa-sort-alpha-down"
-              barSortFieldAbbrev="name"
-              barSortField="nameLowercasedForSorting"
-              barSortFieldText="name"
-            />
-            <SortByFieldBar
-              taskList={taskList}
-              onChangeSort={props.onChangeSort}
-              listTasksPrimarySortField={props.listTasksPrimarySortField}
-              listTasksPrimarySortDirection={
-                props.listTasksPrimarySortDirection
-              }
-              barSortIcon="fas fa-calendar-plus"
-              barSortFieldAbbrev="creation-time"
-              barSortField="creationTime"
-              barSortFieldText="creation time"
-            />
-            <SortByFieldBar
-              taskList={taskList}
-              onChangeSort={props.onChangeSort}
-              listTasksPrimarySortField={props.listTasksPrimarySortField}
-              listTasksPrimarySortDirection={
-                props.listTasksPrimarySortDirection
-              }
-              barSortIcon="fas fa-calendar-check"
-              barSortFieldAbbrev="modified-time"
-              barSortField="modifiedTime"
-              barSortFieldText="modification time"
-            />
-          </div>
-        </FocusTrap>
+        <div
+          className={`single-list-page-menu sorting-menu ${screenSizeClassName}`}
+        >
+          <SortByHeaderBar
+            taskList={taskList}
+            onChangeMenuModeType={props.onChangeMenuModeType}
+          />
+          <SortByFieldBar
+            taskList={taskList}
+            onChangeSort={props.onChangeSort}
+            listTasksPrimarySortField={props.listTasksPrimarySortField}
+            listTasksPrimarySortDirection={props.listTasksPrimarySortDirection}
+            barSortIcon="fas fa-exclamation-circle"
+            barSortFieldAbbrev="priority"
+            barSortField="priority"
+            barSortFieldText="priority"
+          />
+          <SortByFieldBar
+            taskList={taskList}
+            onChangeSort={props.onChangeSort}
+            listTasksPrimarySortField={props.listTasksPrimarySortField}
+            listTasksPrimarySortDirection={props.listTasksPrimarySortDirection}
+            barSortIcon="fas fa-hourglass-start"
+            barSortFieldAbbrev="deadline"
+            barSortField="deadline"
+            barSortFieldText="deadline"
+          />
+          <SortByFieldBar
+            taskList={taskList}
+            onChangeSort={props.onChangeSort}
+            listTasksPrimarySortField={props.listTasksPrimarySortField}
+            listTasksPrimarySortDirection={props.listTasksPrimarySortDirection}
+            barSortIcon="fas fa-sort-alpha-down"
+            barSortFieldAbbrev="name"
+            barSortField="nameLowercasedForSorting"
+            barSortFieldText="name"
+          />
+          <SortByFieldBar
+            taskList={taskList}
+            onChangeSort={props.onChangeSort}
+            listTasksPrimarySortField={props.listTasksPrimarySortField}
+            listTasksPrimarySortDirection={props.listTasksPrimarySortDirection}
+            barSortIcon="fas fa-calendar-plus"
+            barSortFieldAbbrev="creation-time"
+            barSortField="creationTime"
+            barSortFieldText="creation time"
+          />
+          <SortByFieldBar
+            taskList={taskList}
+            onChangeSort={props.onChangeSort}
+            listTasksPrimarySortField={props.listTasksPrimarySortField}
+            listTasksPrimarySortDirection={props.listTasksPrimarySortDirection}
+            barSortIcon="fas fa-calendar-check"
+            barSortFieldAbbrev="modified-time"
+            barSortField="modifiedTime"
+            barSortFieldText="modification time"
+          />
+        </div>
       )}
     </Fragment>
   );

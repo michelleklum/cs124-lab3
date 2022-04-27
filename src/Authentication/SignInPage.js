c6a9d49e87a52c79b2f40f0d57e0bc27da5d9679
Zoe Tokheim <zoetokheim@gmail.com>
2022-04-27 13:26:23 -0700

    google error message

--- src/Authentication/SignInPage.js
+++ src/Authentication/SignInPage.js
@@ -25,7 +25,8 @@ function SignInPage(props) {
        (emailError.message.includes("too-many-requests") ? 
        "Too many login attempts. Your account has been temporarily disabled. \
        Reset your password or try again later." :
-       "We've encountered an error logging you in. Try again.")))) : ""
+       "We've encountered an error logging you in. Try again.")))) : 
+       (googleError ? "We've encountered an error logging you in. Try again." : "")
 
   const [showUsageAlert, setShowUsageAlert] = useState(false);
 

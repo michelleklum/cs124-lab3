49d5a8bb62e39991bff95764ab201ace14cdca1a
mlum123 <michellelum123@gmail.com>
2022-05-03 19:34:21 -0700

    Fix for bug where other user deletes the list that current user is on

--- src/SingleListLoadingPage/SingleListLoadingPage.js
+++ src/SingleListLoadingPage/SingleListLoadingPage.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from "react";
 import ListTopBar from "../SingleListPage/ListTopBar";
-import ListMenu from "../SingleListPage/ListMenu";
 
 function SingleListLoadingPage(props) {
   return (
@@ -12,37 +11,6 @@ function SingleListLoadingPage(props) {
           onChangePage={props.onChangePage}
           isLoading={true}
         />
-        {!props.isLargeScreen &&
-        props.inMenuMode &&
-        props.menuModeType === "general" ? (
-          <ListMenu
-            isLargeScreen={props.isLargeScreen}
-            tasks={[]}
-            listMenuType="general"
-            onChangeMenuModeType={props.setMenuModeType}
-            user={props.user}
-            data={props.data}
-            currentListId={props.currentListId}
-            onEditList={props.onEditList}
-          />
-        ) : null}
-        {!props.isLargeScreen &&
-        props.inMenuMode &&
-        props.menuModeType === "sorting" ? (
-          <ListMenu
-            listMenuType="sorting"
-            onChangeMenuModeType={props.setMenuModeType}
-            user={props.user}
-            data={props.data}
-            currentListId={props.currentListId}
-            listTasksPrimarySortField={props.listTasksPrimarySortField}
-            listTasksPrimarySortDirection={props.listTasksPrimarySortDirection}
-            onChangeSort={props.onChangeSort}
-          />
-        ) : null}
-        <div
-          className={props.inMenuMode ? "single-list-menu-mode-overlay" : null}
-        ></div>
       </div>
     </Fragment>
   );
